3244050e0a0d6e233d6fd72e40b04efceb7c9734
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-01 01:07:05 +0600

    Добавлен CRUD

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,13 @@
-"""core URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from products.views import products_view, product_view, product_add_view, product_delete_view, product_edit_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', products_view, name='products'),
+    path('product/<int:pk>/', product_view, name='product'),
+    path('product/add/', product_add_view, name='product_add'),
+    path('product/edit/<int:pk>', product_edit_view, name='product_edit'),
+    path('product/delete/<int:pk>', product_delete_view, name='product_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from itertools import product
+from django.shortcuts import render, get_object_or_404, redirect
+from products.models import Product
+from django.core.handlers.wsgi import WSGIRequest
+from products.forms import ProductForm
 
-# Create your views here.
+def products_view(request):
+    products = Product.objects.filter(balance__gt=0).order_by('category', 'name')
+    context = {
+        'products': products
+    }
+    return render(request=request, template_name='products.html', context=context)
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request=request, template_name='product.html', context=context)
+
+def product_add_view(request:WSGIRequest):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if not form.is_valid():
+            context = {
+            'choices': Product.CATEGORY_CHOICES,
+            'form': form
+            }  
+            return render(request=request, template_name='product_add.html', context=context)
+
+        product: Product = Product.objects.create(**form.cleaned_data)
+        return redirect('product', pk=product.pk)
+        
+    form = ProductForm()
+    context = {
+        'choices': Product.CATEGORY_CHOICES,
+        'form': form
+    }    
+    return render(request=request, template_name='product_add.html', context=context)
+
+def product_edit_view(request, pk):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=pk)
+        form = ProductForm(request.POST, instance=product)
+        if not form.is_valid():
+            context = {
+            'choices': Product.CATEGORY_CHOICES,
+            'form': form
+            }  
+            return render(request=request, template_name='product_edit.html', context=context)
+
+        form.save()
+        return redirect('product', pk=product.pk)
+        
+    product = get_object_or_404(Product, pk=pk)
+    form = ProductForm(instance=product)
+    context = {
+        'form': form
+    }
+    return render(request=request, template_name='product_edit.html', context=context)
+
+
+def product_delete_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    product.delete()
+    return redirect('products')
