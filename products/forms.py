fc5ff7967b18e2d3d497f1fe190a298bf8809ad4
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-03 00:15:01 +0600

    Поиск по названию продуктов

--- products/forms.py
+++ products/forms.py
@@ -3,16 +3,12 @@ from django import forms
 from products.models import Product
 from django.forms import ModelForm
 
-# class ProductForm(forms.Form):
-#     name = forms.CharField(max_length=100, required=True, label='Наименование')
-#     description = forms.CharField(widget=forms.Textarea, max_length=2000, required=False, label='Описание')
-#     photo = forms.CharField(widget=forms.Textarea, max_length=2000, required=False, label='Фото')
-#     category = forms.ChoiceField(choices=Product.CATEGORY_CHOICES, required=True, label='Категория',)
-#     balance = forms.IntegerField(min_value=0, label='Остаток')
-#     price = forms.DecimalField(max_digits=7, decimal_places=2, label='Стоимость')
-
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'photo', 'category', 'balance', 'price']
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField()
--- products/views.py
+++ products/views.py
@@ -1,13 +1,24 @@
-from itertools import product
 from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Product
 from django.core.handlers.wsgi import WSGIRequest
-from products.forms import ProductForm
+from products.forms import ProductForm, SearchForm
 
-def products_view(request):
-    products = Product.objects.filter(balance__gt=0).order_by('category', 'name')
+def products_view(request: WSGIRequest):
+    search = request.GET.get('search')
+    if search:
+        form = SearchForm(request.GET)
+
+        products = Product.objects.filter(balance__gt=0).order_by('category', 'name').filter(name__iregex=search)
+    else:
+        products = Product.objects.filter(balance__gt=0).order_by('category', 'name')
+        search = ''
+        form = SearchForm()
+
+        
     context = {
-        'products': products
+        'products': products,
+        'search': search,
+        'form': form
     }
     return render(request=request, template_name='products.html', context=context)
 
